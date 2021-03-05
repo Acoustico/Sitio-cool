b33d09edf7a85b46f69032b787bd1103cbcaf05d
Acoustico <migueldiez1@hotmail.com>
2021-03-04 17:08:31 -0800

    sig clase

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -9,6 +9,14 @@ router.get('/', (req,res) =>{
 router.get('/Game', (req,res) =>{
     res.json(juegos);
 })
+router.get('/:ID',(req,res)=>{
+    const {ID}=req.params;
+    juegos.forEach(juego=>{
+        if(juego.ID==ID){
+            req.json(juego);
+        }
+    })
+})
 router.get('/Player',(req,res)=>{
     res.json({"Score":"Score",
             "Total":"Total"});
