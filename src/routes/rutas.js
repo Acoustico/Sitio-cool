162235d55d7a4f4e787bcec01a771dc08a49d8fa
Acoustico <migueldiez1@hotmail.com>
2021-03-04 16:47:25 -0800

    sig clase

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,16 +1,13 @@
 const {Router}=require('express');
 const router=Router();
 
+const juegos=require('./data.json');
+
 router.get('/', (req,res) =>{
     res.json({"Damn":"Daniel"});
 })
 router.get('/Game', (req,res) =>{
-    res.json({"GameID":"ID",
-            "Name":"Name",
-            "Language":"Language",
-            "Scores":"Scores",
-            "Players":"Players",
-            "Ranking":"Ranking"});
+    res.json(juegos);
 })
 router.get('/Player',(req,res)=>{
     res.json({"Score":"Score",
