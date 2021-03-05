7ffc8145139447f7c557c7858c8f7de045710459
Acoustico <migueldiez1@hotmail.com>
2021-03-04 17:09:38 -0800

    sig clase

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -22,4 +22,16 @@ router.get('/Player',(req,res)=>{
             "Total":"Total"});
 })
 
+router.post('/', (req,res)=>{
+    const {Name,Language,Scores,Players,Ranking}=req.body;
+    if(Name){
+        const id=juegos.lenght +1;
+        const newgame = {...req.body,id};
+        juegos.push(newgame);
+        res.status(200).json(juegos);
+    }else{
+        res.status(500).json({error:'no data'});
+    }
+})
+
 module.exports=router;
