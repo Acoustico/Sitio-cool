7db21482ee0ee8dcbd4b0d4ac5556e979c44627b
Acoustico <migueldiez1@hotmail.com>
2021-03-09 16:14:53 -0800

    fornai

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -3,29 +3,35 @@ const router=Router();
 
 const juegos=require('./data.json');
 const pdata=require('./pdata.json');
+const scores=require('./scores.json');//ver como vamos a clasificar los scores
 
 router.get('/', (req,res) =>{
     res.json({"Damn":"Daniel"});
 })
 
 router.get('/Game', (req,res) =>{
-    res.json(juegos); //en las secciones de scores y players tenemos que llamar todas las instancias
+    res.json(juegos); 
 })
 
 router.get('/player',(req,res)=>{
     res.json(pdata);
 })
 
-router.get('/id',(req,res)=>{
+/*router.get('/id',(req,res)=>{
     const {id}=req.params;
     juegos.forEach(juego=>{
         if(juego.ID==id){
             res.json(juego);
         }
     })
-})
+})*/
 
-router.post('/', (req,res)=>{
+router.get('/Game/Scores',(req,res)=>{
+    res.json(scores);
+})//hay que ver si vamos a separar los scores de los playes
+
+
+/*router.post('/', (req,res)=>{
     const {Name,Language,Scores,Players,Ranking}=req.body;
     if(Name){
         const id=juegos.lenght +1;
@@ -35,6 +41,6 @@ router.post('/', (req,res)=>{
     }else{
         res.status(500).json({error:'no data'});
     }
-})
+})*/
 
 module.exports=router;
